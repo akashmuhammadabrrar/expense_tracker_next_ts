818dbfd33670323774ddeab486d2b51c21f9a5ef
akashmuhammadabrrar <madborakash48@gmail.com>
2025-08-11 01:53:18 +0600

    this commit for successfullyl created schema for prisma and connected

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div className='text-3xl text-center mt-5 bg-red-500'>HomePage</div>
+    <div className='text-3xl text-center mt-5'>HomePage</div>
   )
 }
